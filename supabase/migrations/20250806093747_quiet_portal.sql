004cef00a1de9d1625133d182b68e3853cb90de4
patrickbolt3 <patrickbolt3@startmail.com>
2025-08-06 10:37:51 +0100

    Update Pure Awareness to darker color

---
+++ supabase/migrations/20250806093747_quiet_portal.sql
@@ -0,0 +1,9 @@
+@@ .. @@
+ UPDATE harmonic_states 
+ SET color = '#D4AF37' 
+ WHERE name = 'Willingness';
+ 
+ UPDATE harmonic_states 
+-SET color = '#E5E7EB' 
++SET color = '#6B7280' 
+ WHERE name = 'Pure Awareness';
